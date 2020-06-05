356df521cd0eb181dea704dcd69df23a7c27329f
root <root@laptop-xingxiaorui.localdomain>
2020-06-06 05:51:01 +0800

    path

--- games-logger.sh
+++ games-logger.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+export DENO_INSTALL="/root/.deno"
+export PATH=$DENO_INSTALL/bin:/mnt/c/npc/:/mnt/c/Windows/System32:/mnt/c/Windows:$PATH
+
 cd /root/workspace/deno-game-monitor
 deno run --allow-net --allow-run ./games-logger.ts /tmp/games.log
