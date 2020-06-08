470872456d5c8fecb9d057bdfaa9d60cc5b7c389
linuxing3 <linuxing3@qq.com>
2020-06-08 01:50:56 -0400

    optimize

--- deps.ts
+++ deps.ts
@@ -1,20 +1,12 @@
-export {
-  cron,
-  daily,
-  monthly,
-  weekly,
-} from "https://deno.land/x/deno_cron/cron.ts";
-export { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v0.4.0/mod.ts";
 export { slugify } from "https://deno.land/x/slugify@0.3.0/mod.ts";
 export { makeJwt } from "https://deno.land/x/djwt@v0.9.0/create.ts";
-export { hash, compare } from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 export { validateJwt } from "https://deno.land/x/djwt@v0.9.0/validate.ts";
+export { hash, compare } from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 export { v4 } from "https://deno.land/std/uuid/mod.ts";
 export { program, Lizard } from "https://deno.land/x/denomander/mod.ts";
-export { oakCors } from "https://deno.land/x/cors/mod.ts";
-
 export { readJsonSync, writeJsonSync } from "https://deno.land/std/fs/mod.ts";
+export { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
 export {
   bold,
@@ -30,6 +22,8 @@ export {
   Status,
   isHttpError,
 } from "https://deno.land/x/oak/mod.ts";
+export { oakCors } from "https://deno.land/x/cors/mod.ts";
+
 export {
   dso,
   Client,
@@ -39,3 +33,10 @@ export {
   FieldType,
   Field,
 } from "https://deno.land/x/dso@v1.0.0/mod.ts";
+
+export {
+  cron,
+  daily,
+  monthly,
+  weekly,
+} from "https://deno.land/x/deno_cron/cron.ts";
