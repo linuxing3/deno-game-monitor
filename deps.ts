b52e1c949b6909e57aa102241d7cd52b8c60ca16
wenju xing <linuxing3@qq.com>
2020-06-11 23:56:51 +0000

    skip mongo client

--- deps.ts
+++ deps.ts
@@ -36,4 +36,3 @@ export { hash, compare } from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 export { v4 } from "https://deno.land/std/uuid/mod.ts";
 export { program, Lizard } from "https://deno.land/x/denomander/mod.ts";
 export { readJsonSync, writeJsonSync } from "https://deno.land/std/fs/mod.ts";
-export { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
