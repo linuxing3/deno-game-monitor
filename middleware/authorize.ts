156bd0ec9fdbca0ae91b884a35e139003140c799
wenju xing <linuxing3@qq.com>
2020-06-12 14:23:27 +0000

    localstorage not available

--- middleware/authorize.ts
+++ middleware/authorize.ts
@@ -3,22 +3,50 @@ import { Status, validateJwt, config } from "../deps.ts";
 const env = config();
 
 export default async (ctx: any, next: any) => {
-  // FIXED: Check authorization
-  const authHeader = ctx.request.headers.get("authorization");
-
-  if (!authHeader) {
-    ctx.throw(Status.Unauthorized);
-  } else {
-    const token = authHeader.split(" ")[1];
-
-    try {
-      const { payload }: any = await validateJwt(token, env["TOKEN_SECRET"]);
-
-      ctx.request.user = payload;
-
-      await next();
-    } catch (err) {
-      ctx.throw(Status.Unauthorized);
+    // FIXED: Check authorization
+    const authHeader = ctx.request.headers.get("authorization");
+    if (!authHeader) {
+        ctx.throw(Status.Unauthorized);
+    } else {
+        const requestToken = authHeader.split(" ")[1];
+        try {
+            const jwt: any = await validateJwt(requestToken, env["TOKEN_SECRET"]);
+            ctx.request.user = jwt.payload
+            await next();
+        } catch (err) {
+            ctx.throw(Status.Unauthorized);
+        }
     }
-  }
 };
+
+// export const sampleAuth = async (ctx: any, next: any) => {
+//   // local token check  
+//   // const { payload, token } = JSON.parse(localStorage.getItem('token'));
+//   // if no local identity is not available, check request header for token 
+//   if (!payload) {
+//     // FIXED: Check authorization
+//     const authHeader = ctx.request.headers.get("authorization");
+//     if (!authHeader) {
+//         ctx.throw(Status.Unauthorized);
+//     } else {
+//         const requestToken = authHeader.split(" ")[1];
+//         try {
+//             const jwt: any = await validateJwt(requestToken, env["TOKEN_SECRET"]);
+//             localStorage.setItem('token', JSON.stringify({ payload: jwt.payload, token: requestToken }));
+//             await next();
+//         } catch (err) {
+//             ctx.throw(Status.Unauthorized);
+//         }
+//     }
+//   } else {
+//       // if local identity is not available, check cached token 
+//         try {
+//             const jwt: any = await validateJwt(token, env["TOKEN_SECRET"]);
+//             localStorage.setItem('token', JSON.stringify({ payload: jwt.payload, token }));
+//             await next();
+//         } catch (err) {
+//             ctx.throw(Status.Unauthorized);
+//         }
+//   }
+
+// };
