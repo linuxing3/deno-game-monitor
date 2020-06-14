c5b60243cc16166f2e9a7bea8dfb55c58c35cdc4
linuxing3 <linuxing3@qq.com>
2020-06-14 16:14:22 -0400

    deleted unuseful

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,18 +1,9 @@
 // deno-lint-ignore-file
 // @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
-import { React, ReactRouter } from "../deps.ts";
-import ListComponent from "./list.tsx";
-import LoginComponent from "./form.tsx";
-import { ComponentMap } from "../helpers/render.ts";
-
-const {
-  Route,
-  Switch,
-  NavLink,
-  BrowserRouter,
-  Router,
-  StaticRouter,
-} = ReactRouter;
+import { React, ReactRouter } from '../deps.ts';
+import ListComponent from './list.tsx';
+import LoginComponent from './form.tsx';
+import { ComponentMap } from '../helpers/render.ts';
 
 declare global {
   namespace JSX {
@@ -29,74 +20,36 @@ declare global {
       label: any;
       input: any;
       LoginComponent: any;
-      Route: any;
-      NavLink: any;
-      Switch: any;
-      Router: any;
-      StaticRouter: any;
-      BrowserRouter: any;
     }
   }
 }
 
-const Home = (props: any) => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/todos">Todos</NavLink>
-        </li>
-        <li>
-          <NavLink to="/posts">Posts</NavLink>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path="/" render={() => <div>home</div>} />
-        <Route path="/todos" component={ListComponent} />
-        <Route path="/posts" component={LoginComponent} />
-      </Switch>
-    </div>
-  );
-};
-
 const App = (props: any) => {
   return (
-    <div class="flex">
-      <div class="w-2/5">
+    <div class='flex'>
+      <div class='w-2/5'>
         <LoginComponent />
       </div>
-      <div class="w-3/5">
+      <div class='w-3/5'>
         <ListComponent />
       </div>
     </div>
   );
 };
 
-export const AppWithRouter = () => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-};
-
 export const componentMapList: ComponentMap[] = [
   {
-    name: "App",
-    component: App,
+    name: 'App',
+    component: App
   },
   {
-    name: "LoginComponent",
-    component: LoginComponent,
+    name: 'LoginComponent',
+    component: LoginComponent
   },
   {
-    name: "ListComponent",
-    component: ListComponent,
-  },
+    name: 'ListComponent',
+    component: ListComponent
+  }
 ];
 
 export default App;
