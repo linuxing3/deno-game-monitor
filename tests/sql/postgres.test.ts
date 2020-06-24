837a3f1ab60e270e9ca6b8f1140397bfe5a8ae78
linuxing3 <linuxing3@qq.com>
2020-06-23 22:04:15 -0400

    Commited by linuxing3 from /home/linuxing3/deno-game-monitor on 2020-06-23-22:04

---
+++ tests/sql/postgres.test.ts
@@ -0,0 +1,107 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { DataTypes, Database, Model } from "https://deno.land/x/denodb/mod.ts";
+
+const db = new Database("postgres", {
+  host: "dongxishijie.xyz",
+  username: "postgres",
+  password: "20090909",
+  database: "monitor",
+  port: 9006,
+});
+
+const flights = [
+  {
+    departure: "Paris",
+    destination: "Tokyo",
+  },
+  {
+    departure: "Caracas",
+    destination: "Beijing",
+  },
+  {
+    departure: "Shanghai",
+    destination: "New York",
+  },
+];
+
+class Flight extends Model {
+  static table = "flights";
+  static timestamps = true;
+
+  static fields = {
+    id: { primaryKey: true, autoIncrement: true },
+    departure: DataTypes.STRING,
+    destination: DataTypes.STRING,
+    flightDuration: DataTypes.FLOAT,
+  };
+
+  static defaults = {
+    flightDuration: 2.5,
+  };
+}
+
+db.link([Flight]);
+
+await db.sync({ drop: true });
+
+flights.forEach(async (flight) => {
+  await Flight.create(flight);
+});
+
+// or
+// const flight = new Flight();
+// flight["departure"] = "London";
+// flight["destination"] = "San Francisco";
+// await flight.save();
+
+// getAll
+Deno.test("[ Denodb ]: all flights", async () => {
+  const records = await Flight.select("destination").all();
+  const expected = [
+    { destination: "Tokyo" },
+    { destination: "Beijing" },
+    { destination: "New York" },
+  ];
+  assertEquals(records, expected);
+});
+
+Deno.test("[ Denodb ]: get second flight", async () => {
+  const sfFlight = await Flight.select("destination").find(2);
+  const expected = { destination: "Beijing" };
+  assertEquals(sfFlight, expected);
+  // console.log(sfFlight);
+});
+
+Deno.test("[ Denodb ]: find one flight by id", async () => {
+  const foundRecord = await Flight.select("id", "destination").orderBy("id")
+    .get();
+  const expected = [
+    { id: 1, destination: "Tokyo" },
+    { id: 2, destination: "Beijing" },
+    { id: 3, destination: "New York" },
+  ];
+  assertEquals(foundRecord, expected);
+});
+
+Deno.test("[ Denodb ]: update one flight by id", async () => {
+  const newFlight = {
+    departure: "Paris",
+    destination: "Beijing",
+  };
+  await Flight.where("destination", "Tokyo").update(
+    { destination: "Beijing" },
+  );
+  const updatedRecord = await Flight.where("destination", "Beijing").orderBy(
+    "id",
+  ).get();
+  assertEquals(updatedRecord[0]["id"], 1);
+  assertEquals(updatedRecord[0]["destination"], "Beijing");
+});
+
+// count
+Deno.test("[ Denodb ]: update one flight by id", async () => {
+  const total = await Flight.count();
+  assertEquals(parseInt(total), 3);
+});
+
+await db.close();
