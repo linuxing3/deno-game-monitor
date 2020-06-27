db629a76006043ba20b2a4caa4e010c2c945df91
linuxing3 <linuxing3@qq.com>
2020-06-27 02:00:55 -0400

    more examples

---
+++ exec/sequence.ts
@@ -0,0 +1,11 @@
+import { exec, execSequence, OutputMode } from "https://deno.land/x/exec/mod.ts";
+
+await exec('echo Running Sequence');
+
+let response = await execSequence([
+  "echo Hello World",
+  "ls -l",
+  "cat deno.json",
+], { output: OutputMode.StdOut, continueOnError: false });
+
+console.log(response);
