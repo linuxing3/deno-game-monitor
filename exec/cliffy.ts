5fd088bb89d3db97ecee9885bf08db99f87c395b
linuxing3 <linuxing3@qq.com>
2021-01-16 19:39:26 -0400

    drash examples

--- exec/cliffy.ts
+++ exec/cliffy.ts
@@ -1,32 +1,50 @@
-import { Table } from "https://deno.land/x/cliffy/table.ts";
-import { Command } from "https://deno.land/x/cliffy/command.ts";
-import { Input } from "https://deno.land/x/cliffy/prompt.ts";
-import { Confirm } from "https://deno.land/x/cliffy/prompt.ts";
-import { Select, Separator } from "https://deno.land/x/cliffy/prompt.ts";
+import { Command } from "https://deno.land/x/cliffy/command/mod.ts";
+import { prompt, Input, Number, Confirm, Checkbox } from "https://deno.land/x/cliffy/prompt/mod.ts";
+import { Table } from "https://deno.land/x/cliffy/table/mod.ts";
 
-const pizza: string = await Select.prompt({
-  message: "Select your pizza?",
-  options: [{
-    name: "", value: "taste"
-  }],
-});
+// const { options } = await new Command()
+//   .option("-s, --silent", "disable output.")
+//   .option("-d, --debug [level]", "output extra debugging.")
+//   .option("-p, --port <port>", "the port number.")
+//   .option("-h, --host [hostname]", "the host name.", { default: "localhost" })
+//   .option("-a, --allow [hostname]", "the host name.", { default: "localhost" })
+//   .parse(Deno.args);
 
-const useDeno: boolean = await Confirm.prompt("Would you like to use deno?");
-const name: string = await Input.prompt(`What's your command name?`);
+// console.log("server running at %s:%s", options.host, options.port);
 
-await new Command()
-  .version("0.1.0")
-  .description(`${name} Example description ...`)
-  .parse(Deno.args);
+const result = await prompt([{
+  name: "name",
+  message: "What's your name?",
+  type: Input,
+}, {
+  name: "age",
+  message: "How old are you?",
+  type: Number,
+}, {
+  name: "like",
+  message: "Do you like animal's?",
+  type: Confirm,
+}, {
+  name: "animals",
+  message: "Select some animal's",
+  type: Checkbox,
+  options: ["dog", "cat", "snake"],
+}]);
 
-Table.from([
-  ["Flags", "Description", "Types"],
-  ["cell1", "cell2", "cell3"],
-  ["cell1", "cell2", "cell3"],
-  ["cell1", "cell2", "cell3"],
-])
-  .maxCellWidth(50)
-  .padding(1)
-  .indent(2)
-  .border(true)
-  .render();
+console.log(result);
+
+const table: Table = new Table(
+  ["Row 1 Column 1", "Row 1 Column 2", "Row 1 Column 3"],
+  ["Row 2 Column 1", "Row 2 Column 2", "Row 2 Column 3"],
+  ["Row 3 Column 1", "Row 3 Column 2", "Row 3 Column 3"],
+);
+
+table
+.padding(1)
+.indent(2)
+.border(true)
+.render();
+
+// console.log(table.toString());
+// You can also use table.render() as shorthand which uses Deno.stdout.writeSync() under the hood.
+
