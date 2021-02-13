218122622881f57036aaf5ca13216dea96746976
linuxing3 <linuxing3@qq.com>
2021-02-13 00:40:55 -0400

    use trex

--- tests/sql/create_table_mysql.ts
+++ tests/sql/create_table_mysql.ts
@@ -11,13 +11,13 @@ import {
 
 const mydb = new Database("mysql", {
   host: "xunqinji.top",
-  username: "monitor",
-  password: "20090909",
-  database: "monitor",
-  port: 9005
+  username: "root",
+  password: "jGPcf",
+  database: "xray",
+  port: 9203
 });
 
-mydb.link([User, Member, Document, Vehicle, Militant, Project, Flight]);
+mydb.link([Member, Document, Vehicle, Militant, Project, Flight]);
 
 await mydb.sync({ drop: true });
 
